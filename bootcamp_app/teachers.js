544bca61832dbbdb180bcdb6dc63e86677c07d6e
Peter Heyboer <heyboer.peter@gmail.com>
2024-12-10 18:44:19 -0500

    parameterized query for SQL injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,6 +7,29 @@ const pool = new Pool({
   database: 'bootcampx',
 });
 
+const cohortName = process.argv[2] || 'JUL02';
+
+const queryString = `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name = $1
+  ORDER BY teacher;
+  `;
+
+const values = [cohortName];
+
+pool
+  .query(queryString, values)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  })
+  .catch(err => console.error('Query Error', err.stack));
+
 // pool
 //   .query(
 //     `
@@ -20,18 +43,3 @@ const pool = new Pool({
 //     console.log(res.rows);
 //   })
 //   .catch(err => console.error('query error', err.stack));
-
-pool.query(`
-  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-  FROM teachers
-  JOIN assistance_requests ON teacher_id = teachers.id
-  JOIN students ON student_id = students.id
-  JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-  ORDER BY teacher;
-  `)
-  .then(res => {
-    res.rows.forEach(row => {
-      console.log(`${row.cohort}: ${row.teacher}`);
-    })
-  });
